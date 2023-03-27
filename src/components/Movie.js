6e12717a29657707c9003b9ffb78b2a1f19a77bf
yurisokolovicz <yurisokolovicz@gmail.com>
2023-03-27 14:15:27 -0300

    initial adjustments

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,15 +1,15 @@
 import React from 'react';
 
-import classes from './Movie.module.css';
+import styles from './Movie.module.css';
 
-const Movie = (props) => {
-  return (
-    <li className={classes.movie}>
-      <h2>{props.title}</h2>
-      <h3>{props.releaseDate}</h3>
-      <p>{props.openingText}</p>
-    </li>
-  );
+const Movie = props => {
+    return (
+        <li className={styles.movie}>
+            <h2>{props.title}</h2>
+            <h3>{props.releaseDate}</h3>
+            <p>{props.openingText}</p>
+        </li>
+    );
 };
 
 export default Movie;
--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -1,21 +1,16 @@
 import React from 'react';
 
 import Movie from './Movie';
-import classes from './MoviesList.module.css';
+import styles from './MoviesList.module.css';
 
-const MovieList = (props) => {
-  return (
-    <ul className={classes['movies-list']}>
-      {props.movies.map((movie) => (
-        <Movie
-          key={movie.id}
-          title={movie.title}
-          releaseDate={movie.releaseDate}
-          openingText={movie.openingText}
-        />
-      ))}
-    </ul>
-  );
+const MovieList = props => {
+    return (
+        <ul className={styles['movies-list']}>
+            {props.movies.map(movie => (
+                <Movie key={movie.id} title={movie.title} releaseDate={movie.releaseDate} openingText={movie.openingText} />
+            ))}
+        </ul>
+    );
 };
 
 export default MovieList;
